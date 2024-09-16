de29344150f79e9a0824eb51575866f25a2208f0
kn911 <kn911@MacBook-Air-kn911.local>
2024-09-16 20:44:34 +0300

    id заметки +1 от предыдущей

--- app/handlers/create_note.py
+++ app/handlers/create_note.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Blueprint, request, render_template, redirect, url_for, make_response, jsonify
+from flask import Blueprint, request, render_template, redirect, url_for
 import jwt
 from .login import load_data, save_data
 from config import SECRET_KEY
@@ -23,8 +23,17 @@ def verify_jwt_cookie():
     return None
 
 
+def get_last_id(notes):
+    if notes:
+        return max(int(note_id) for note_id in notes.keys())
+    return 0
+
+
 @create_note_route.route('/create_note', methods=["GET", 'POST'])
 def create_note():
+    notes = load_data(NOTES_FILE)
+    id = get_last_id(notes)
+
     if request.method == "POST":
         username = verify_jwt_cookie()
         if not username:
@@ -35,21 +44,21 @@ def create_note():
         visibility = request.form.get('visibility', 'private')
         author = username
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        id_ = str(int(datetime.now().timestamp() * 1000))
-        notes = load_data(NOTES_FILE)
+        id += 1
 
         new_note = {
-            "id": id_,
+            "id": str(id),
             'title': title,
             'content': content,
-            'visibility': "public",
+            'visibility': visibility,
             'author': author,
             'timestamp': timestamp
         }
+
         notes[new_note["id"]] = new_note
         save_data(notes, NOTES_FILE)
 
-        return redirect(url_for("view_note_route.view_note", note_id=id_))
+        return redirect(url_for("view_note_route.view_note", note_id=id))
 
     username = verify_jwt_cookie()
     if not username:
